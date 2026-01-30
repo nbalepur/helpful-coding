ea20e36054364c21f71dbfa94ff1b00c632c6567
nbalepur <nbalepur@umd.edu>
2026-01-30 12:29:41 -0500

    fix leaderboard

--- interface/app/config/tasks.ts
+++ interface/app/config/tasks.ts
@@ -6,3 +6,6 @@
 // These should match the task.name field from the API
 export const POST_TEST_REQUIRED_TASKS = ['connect_four', 'snake', 'platformer'] as const;
 
+// Usernames to filter out from the leaderboard (e.g., test accounts, internal users)
+export const LEADERBOARD_FILTERED_USERNAMES: string[] = ['testtest', 'test-example', 'test', 'test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10', 'test11'];
+
