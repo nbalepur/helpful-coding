7310f15835dc72ec1f1b2e2084d5256e5227ebde
nbalepur <nbalepur@umd.edu>
2026-02-05 16:52:34 -0500

    more test accts

--- interface/app/config/tasks.ts
+++ interface/app/config/tasks.ts
@@ -7,5 +7,5 @@
 export const POST_TEST_REQUIRED_TASKS = ['connect_four', 'snake', 'platformer'] as const;
 
 // Usernames to filter out from the leaderboard (e.g., test accounts, internal users)
-export const LEADERBOARD_FILTERED_USERNAMES: string[] = ['testtest', 'test-example', 'test', 'test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10', 'test11'];
+export const LEADERBOARD_FILTERED_USERNAMES: string[] = ['testtest', 'test-example', 'test', 'test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10', 'test11', 'test12', 'test13', 'test14', 'test15', 'test16', 'test17', 'test18', 'test19', 'test20'];
 
